8c2adcd6cf81132356a9457796db7bc0f3eb927d
Erfan Sayyari <esayyari@ucsd.edu>
2019-10-22 12:41:01 -0700

    updating LICENSE

--- q2_feature_engineering/_tada/__init__.py
+++ q2_feature_engineering/_tada/__init__.py
@@ -1,3 +1,10 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2019, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
 from ._tada import tada
 __version__ = "1.0"
 
